d84d4596966448f0c53a448fa8c72a4cc72b07e7
thiago <tjb23@inf.ufpr.br>
2024-11-17 13:20:23 -0300

    atualização principal + busca linear

--- projeto3.py
+++ projeto3.py
@@ -1,6 +1,15 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+'''
+def linesearch(f,x,g,d):
+    alpha = 1
+    while(f(x+alpha*d) > f(x) + alpha*1e-3*d*g(x)):
+        alpha /= 2
+
+    return alpha
+'''
+
 def f(x):
     return x[0]**4-2*x[0]**2+x[0]-x[0]*x[1]+x[1]**2
 
@@ -13,10 +22,21 @@ def gd(f,x0,grad,eps = 1e-5,alpha = 0.1,itmax = 10000,fd = False,h = 1e-7,plot =
     a = x - (alpha * grad(x))
     while (np.linalg.norm(grad(x)) > eps) and (k < itmax):
         k += 1
+        '''
+        if(search):
+            alpha = linesearch(f,x,grad,neg_grad)
+        '''
         x = x - (alpha * grad(x))
-    
+
+    if(plot):
+        img = [np.linspace(-1.5,0.5,1000), np.linspace(-0.7,0.5,1000)]
+        X, Y = np.meshgrid(img[0],img[1])
+        F =  f([X,Y])
+        plt.contour(X,Y,F)   
+        plt.show()
+
     return x, k
 
-x,k = gd(f,np.array([0,0]),grad)
+x,k = gd(f,np.array([0,0]),grad,search=True)
 print(x)
 print(k)
